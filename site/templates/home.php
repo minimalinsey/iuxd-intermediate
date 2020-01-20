83ea3eb66f1c917146c4bb492de19bda5e0bc37f
minimalinsey <48240294+minimalinsey@users.noreply.github.com>
2020-01-20 17:09:51 +0100

    Fixed the things

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,102 +7,143 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/hone.css">
+    <link rel="stylesheet" href="assets/css/home.css">
     <title>Service Blueprint</title>
   </head>
   <body>
     
 <main>
   <article class="note">
-    <header class="note-header intro">
-      <h1><?= $page->title() ?></h1>
-      <time class="note-date"><?= $page->date()->toDate('d F Y') ?></time>
-      <?php if ($page->tags()->isNotEmpty()) : ?>
-      <p class="note-tags tags"><?= $page->tags() ?></p>
-      <?php endif ?>
-    </header>
+    <header class="note-header intro"></header>
 
-    <section class="container">
-        <div class="row">
-            <div class="col-11">
-                kolom 1
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="header-title">
+                <h1><?= $page->title() ?></h1>
+                <time class="note-date"><?= $page->date()->toDate('d F Y') ?></time>
+                <?php if ($page->tags()->isNotEmpty()) : ?>
+                <p class="note-tags tags"><?= $page->tags() ?></p>
                 <?= $page->IntroText()->kt() ?>
-                <?= $page->WhatTitle()->kt() ?>
-                <?= $page->WhatText()->kt() ?>
-                <?= $page->WhatTitleTwo()->kt() ?>
-                <?= $page->WhatTextTwo()->kt() ?>
-                <?= $page->WhenText()->kt() ?>
-                <?= $page->HowTitle()->kt() ?>
-                <?= $page->HowText()->kt() ?>
+                <?php endif ?>
             </div>
         </div>
+    </div>
 
+    <section class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="what">
+                    <?= $page->WhatTitle()->kt() ?>
+                    <?= $page->WhatText()->kt() ?>
+                </div>
+                <div class="whattwo">
+                    <?= $page->WhatTitleTwo()->kt() ?>
+                    <?= $page->WhatTextTwo()->kt() ?>
+                     <div class="when">
+                        <?= $page->WhenText()->kt() ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-<!--- accordion ---> 
-<section class="container">
+    <!--- accordion ---> 
+    <section class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            <div class="col-md-6 offset-md-3">
+                <div class="how">
+                 <?= $page->HowTitle()->kt() ?>
+                 <?= $page->HowText()->kt() ?>
+                <div class="accordion" id="accordionExample">
+                    <div class="cardone">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <?= $page->TitleFirst()->kt() ?> 
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <?= $page->TextFirst()->kt() ?> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cardtwo">
+                        <div class="card-header" id="headingTwo">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <?= $page->TitleSecond()->kt() ?> 
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <?= $page->TextSecond()->kt() ?> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cardthree">
+                        <div class="card-header" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    <?= $page->TitleThird()->kt() ?> 
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <?= $page->TextThird()->kt() ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cardfour">
+                        <div class="card-header" id="headingFour">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    <?= $page->TitleFourth()->kt() ?> 
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                            <div class="card-body">
+                                    <?= $page->TextFourth()->kt() ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cardfive">
+                        <div class="card-header" id="headingFive">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                    <?= $page->TitleFifth()->kt() ?> 
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+                            <div class="card-body">
+                                    <?= $page->TextFifth()->kt() ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--- accordion ---> 
 
-<div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h2>
-    </div>
+    <section class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="bronnen">
+                    <?= $page->BronnenTitle()->kt() ?>
+                    <?= $page->BronnenText()->kt() ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </button>
-      </h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
-
-      </div>
-      </div>
-      </section>
-<!--- accordion ---> 
-<section class="container">
-<div class="col-12">
-             kolom 2
-             <?= $page->BronnenTitle()->kt() ?>
-             <?= $page->BronnenText()->kt() ?>
- </div>
- </section>
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
